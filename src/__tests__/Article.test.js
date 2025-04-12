5c9eb59f77fd5bb86360bb8a4320cd4609a66752
jonathan <cliffjonathan66@gmail.com>
2025-04-12 21:00:12 +0300

    swoop

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,10 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
-
-import Article from "../components/Article";
+// src/__tests__/Article.test.js
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; // for toBeInTheDocument()
+import Article from '../components/Article'; // adjust if your path is different
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  screen.debug();
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
